bd985374531fada1c12ee167dea498b3791e1324
Rahul <codewithrahul007@gmail.com>
2023-04-04 06:59:23 +0530

    Added clear button and equal button

--- main.js
+++ main.js
@@ -6,6 +6,7 @@
 
   let screen = document.querySelector(".screen");
   let buttons = document.querySelectorAll(".btn");
-
+  let clear = document.querySelector(".btn-clear");
+  let equal = document.querySelector(".btn-equal");
 
 })()
